50e7b3add3ffe649bc8c8a4bdb50aa5be9c5cd21
Pratik Autee <pratikautee@gmail.com>
2023-05-02 13:56:33 -0500

    optimized test case generator

--- SP9/TestCaseGenerator.java
+++ SP9/TestCaseGenerator.java
@@ -1,7 +1,6 @@
 // Project by pga210001, axk190189
 
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
@@ -16,29 +15,27 @@ public class TestCaseGenerator {
             numNodes = Integer.parseInt(args[0]);
             numEdges = Integer.parseInt(args[1]);
         }
-
-        Set<String> inputSet = new HashSet<>();
-
-        Random rng = new Random();
-
-        while (inputSet.size() < numEdges) {
-            int v1 = rng.nextInt(numNodes - 1) + 1;
-            int v2 = rng.nextInt(numNodes - 1) + 1;
-            int wt = rng.nextInt(1000 - 1) + 1;
-            if (v1 != v2 && !inputSet.contains(v1 + " " + v2) && !inputSet.contains(v2 + " " + v1)) {
-                inputSet.add(v1 + " " + v2 + " " + wt);
-            }
-        }
         try {
             String fileName = "mst_" + numNodes + "_" + numEdges + ".txt";
             FileWriter fw = new FileWriter(fileName);
             fw.write(numNodes + " " + numEdges + "\n");
-            for (String edge : inputSet) {
-                fw.write(edge + "\n");
+
+            Set<String> inputSet = new HashSet<>();
+
+            Random rng = new Random();
+
+            while (inputSet.size() < numEdges) {
+                int v1 = rng.nextInt(numNodes - 1) + 1;
+                int v2 = rng.nextInt(numNodes - 1) + 1;
+                int wt = rng.nextInt(1000 - 1) + 1;
+                if (v1 != v2 && !inputSet.contains(v1 + " " + v2) && !inputSet.contains(v2 + " " + v1)) {
+                    String e = v1 + " " + v2 + " " + wt;
+                    inputSet.add(e);
+                    fw.write(e + "\n");
+                }
             }
             fw.close();
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
