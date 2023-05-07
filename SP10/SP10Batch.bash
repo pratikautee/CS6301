5cdea47601d0e9046adaf45f8ad9f5804da4322d
Pratik Autee <pratikautee@gmail.com>
2023-05-07 11:07:09 -0500

    Added test results for all except choice 0

--- SP10/SP10Batch.bash
+++ SP10/SP10Batch.bash
@@ -4,7 +4,7 @@ echo '#####################################################################'
 
 for n in 16000000 32000000 64000000 128000000
 do
-    for choice in 0 3 4 5 6
+    for choice in 3 4 5 6
     do
         echo '############################' N = $n Choice = $choice '#########################################'
         $(which java) -Xmx32g -Xms16g idsa.Msort $n $choice > sp10_$(echo $n)_$(echo $choice).txt
